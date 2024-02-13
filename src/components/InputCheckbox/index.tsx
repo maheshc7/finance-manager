a0c158adc1657bcf9981e85d6b45604fb21959d1
Mahesh <mrak4123@gmail.com>
2024-02-12 20:37:29 -0800

    init

---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames";
+import { useRef } from "react";
+import { InputCheckboxComponent } from "./types";
+
+export const InputCheckbox: InputCheckboxComponent = ({
+  id,
+  checked = false,
+  disabled,
+  onChange,
+}) => {
+  const { current: inputId } = useRef(`KaizntreeInputCheckbox-${id}`);
+
+  return (
+    <div className="KaizntreeInputCheckbox--container" data-testid={inputId}>
+      <label
+        htmlFor={inputId}
+        className={classNames("KaizntreeInputCheckbox--label", {
+          "KaizntreeInputCheckbox--label-checked": checked,
+          "KaizntreeInputCheckbox--label-disabled": disabled,
+        })}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className="KaizntreeInputCheckbox--input"
+        checked={checked}
+        disabled={disabled}
+        onChange={() => onChange(!checked)}
+      />
+    </div>
+  );
+};
